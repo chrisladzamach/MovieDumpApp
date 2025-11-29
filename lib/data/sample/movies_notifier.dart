204a84568c3b516f864464605714e978154bd640
OrichalcosShunnoros <c3146092091@gmail.com>
2025-11-29 18:43:36 -0500

    update: guide ui was created in principal view

--- lib/data/sample/movies_notifier.dart
+++ lib/data/sample/movies_notifier.dart
@@ -1,25 +1,5 @@
 import 'package:flutter/material.dart';
 import '../models/movie.dart';
-import '../models/evaluation_criteria.dart';
 
 /// Lista reactiva que usa ValueNotifier para notificar cambios a la UI.
-ValueNotifier<List<Movie>> moviesNotifier = ValueNotifier<List<Movie>>([
-  Movie(
-    title: "Interestellar",
-    criteria: [
-      EvaluationCriteria(name: "Fotografía / Efectos visuales", score: 10),
-      EvaluationCriteria(name: "Guion", score: 9),
-      EvaluationCriteria(name: "Banda sonora", score: 9),
-      EvaluationCriteria(name: "Actuación", score: 8),
-    ],
-  ),
-  Movie(
-    title: "Avatar",
-    criteria: [
-      EvaluationCriteria(name: "Fotografía / Efectos visuales", score: 10),
-      EvaluationCriteria(name: "Guion", score: 9),
-      EvaluationCriteria(name: "Banda sonora", score: 9),
-      EvaluationCriteria(name: "Actuación", score: 8),
-    ],
-  ),
-]);
+ValueNotifier<List<Movie>> moviesNotifier = ValueNotifier<List<Movie>>([]);
--- lib/features/movie_list/screens/movie_list_screen.dart
+++ lib/features/movie_list/screens/movie_list_screen.dart
@@ -10,6 +10,7 @@ class MovieListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.background,
       appBar: AppBar(
         backgroundColor: Colors.green,
         title: const Text(
@@ -18,22 +19,96 @@ class MovieListScreen extends StatelessWidget {
         ),
       ),
 
-      body: Container(
-        color: AppColors.background,
-        child: ValueListenableBuilder<List<dynamic>>(
-          valueListenable: moviesNotifier,
-          builder: (_, movies, _) {
-            return ListView.builder(
-              padding: const EdgeInsets.all(16),
-              itemCount: movies.length,
-              itemBuilder: (_, i) => MovieCard(movie: movies[i]),
-            );
-          },
-        ),
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ValueListenableBuilder(
+              valueListenable: moviesNotifier,
+              builder: (context, movies, _) {
+                if (movies.isEmpty) {
+                  return Align(
+                    alignment: Alignment.topCenter,
+                    child: Container(
+                      margin: const EdgeInsets.only(top: 40),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 12,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white.withValues(alpha: 0.1),
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: Colors.white.withValues(alpha: 0.2),
+                          width: 1,
+                        ),
+                      ),
+                      child: Column(
+                        children: const [
+                          Text(
+                            "Aún no has añadido ninguna película.\nAñade una para iniciar tu lista.",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 22,
+                              fontWeight: FontWeight.w100,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            "Haz click en el botón para añadir una.",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 28,
+                              fontWeight: FontWeight.w200,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+
+                return ListView.builder(
+                  itemCount: movies.length,
+                  itemBuilder: (context, index) {
+                    final movie = movies[index];
+                    return MovieCard(movie: movie);
+                  },
+                );
+              },
+            ),
+          ),
+
+          ValueListenableBuilder(
+            valueListenable: moviesNotifier,
+            builder: (context, movies, _) {
+              if (movies.isNotEmpty) return SizedBox.shrink();
+
+              return Positioned(
+                bottom: 80,
+                right: 5,
+                child: Column(
+                  children: const [
+                    Icon(
+                      Icons.arrow_downward,
+                      size: 80,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ],
       ),
 
       floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.pushNamed(context, Routes.addMovie),
+        onPressed: () {
+          Navigator.pushNamed(context, Routes.addMovie);
+        },
         backgroundColor: AppColors.buttonColor,
         child: const Icon(Icons.add, color: Colors.white),
       ),
